c2b09f9e928285fd446a5a2fd50ea4218c954e99
MansurTsutiev <mtsutiev@movableink.com>
2022-10-23 19:34:02 -0400

    style the form with tailwind

--- app/controllers/blocks_controller.rb
+++ app/controllers/blocks_controller.rb
@@ -1,8 +1,20 @@
 class BlocksController < ApplicationController
+  def index
+    @blocks = Block.all
+  end 
+
   def new
     @block = Block.new
   end
 
   def create
+    Block.create(blocks_params)
+    redirect_to action: "index"
+  end
+
+  private
+
+  def blocks_params
+    params.require(:block).permit(:link)
   end
 end
